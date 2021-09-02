aa95ae5111a2983a067f36fb4b7ac42c1b452c1a
ME.GE <89516238+bessiki@users.noreply.github.com>
2021-09-02 14:37:20 +0400

    Add files via upload

---
+++ scripts/form-9ebd.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'PASSWORD',
+    'to' => ''
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'email\' is required.'
+    )
+    ),
+    'text' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'text',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'text\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
